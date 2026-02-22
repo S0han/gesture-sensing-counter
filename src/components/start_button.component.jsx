55cebb08c5cd65e58fb95356f6e3972737d6739b
kanepereira <kane.pereira18@gmail.com>
2026-02-22 15:51:33 -0500

    New: Add Start button and Home Screen folders with their compnents

---
+++ src/components/start_button.component.jsx
@@ -0,0 +1,7 @@
+export default function StartButton() {
+    return (
+        <button className="px-6 py-3 bg-blue-600 text-white rounded">
+            Start
+        </button>
+    );
+}
---
+++ src/screens/home.screen.jsx
@@ -0,0 +1,9 @@
+import StartButton from "../components/start_button.component";
+
+export default function Home() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <StartButton />
+        </div>
+    );
+}
