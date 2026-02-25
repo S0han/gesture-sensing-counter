87d8aa383e1b3b4b45461b492578cd5a794175f8
kanepereira <kane.pereira18@gmail.com>
2026-02-24 23:26:08 -0500

    update: clean up the 2 state timer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Home from './screens/home.screen';
 import Detect from './screens/detect.screen';
 
-function App() {
+export default function App() {
   //add enums to represent states within the timer window
   const STATES = {
     IDLE: "IDLE",
@@ -14,22 +14,18 @@ function App() {
 
   //check if start button changed to true value
   useEffect(() => {
-    if (state == STATES.ACTIVE) {
-      console.log("prop drill worked");
-      setTimeout(() => {
-        //change start button state to false after 15sec
-        setState(STATES.IDLE);
-      }, 15000)
+    if (state !== STATES.ACTIVE) {
+      return;
     }
+
+    const timer = setTimeout(() => {
+      setState(STATES.IDLE);
+    }, 15000);
+
+
+    return () => clearTimeout(timer)
   }, [state])
 
-  useEffect(() => {
-    if (state == STATES.IDLE ) {
-      //log that state actually changed back to false after 15sec
-      console.log('15sec Ended Back to Home')
-    }
-  }, [state]);
- 
   return (
     //show the detect screen only when start is true
     <div>
@@ -38,5 +34,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
