d1aae4040a9f448e0ac6cac49a9f764e336d1a21
kanepereira <kane.pereira18@gmail.com>
2026-03-01 16:16:30 -0500

    update: change invalid button to become return to home button

--- src/components/return_home_button.component.jsx
+++ src/components/return_home_button.component.jsx
@@ -1,7 +1,7 @@
-export default function InvalidGestButton() {
+export default function ReturnHomeButton() {
     return (
         <button className="px-6 py-3 bg-red-600 rounded">
-            Invalid
+            Return Home
         </button>
     );
 }
--- src/screens/detect.screen.jsx
+++ src/screens/detect.screen.jsx
@@ -1,11 +1,9 @@
-import ValidGestButton from '../components/valid_gest_button.component';
-import InvalidGestButton from '../components/invalid_gest_button.component'; 
+import ValidGestButton from '../components/valid_gest_button.component'; 
 
 export default function Detect({ setIsValid }) {
     return (
         <div className="flex h-screen items-center justify-center">
             <div className="flex gap-2">
-                <InvalidGestButton />
                 <ValidGestButton setIsValid={setIsValid} />
             </div>
         </div>
