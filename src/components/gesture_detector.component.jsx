5f5bde0b837c7602396d1496a01e4f129dbe23cb
kanepereira <kane.pereira18@gmail.com>
2026-04-03 20:24:12 -0400

    update: complete validation for hand and face gesture and validate in browser 3sec

--- src/components/gesture_detector.component.jsx
+++ src/components/gesture_detector.component.jsx
@@ -31,7 +31,7 @@ export default function GestureDetector({ valDetect }) {
             //for the face
             const faceRecognizer = await FaceLandmarker.createFromOptions(vision, {
                     baseOptions: {
-                        modelAssetPath: "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker.task",
+                        modelAssetPath: "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task"
                     },
                     runningMode: "VIDEO",
                     outputFaceBlendshapes: true
@@ -46,12 +46,22 @@ export default function GestureDetector({ valDetect }) {
     }, []);
 
     const onResults = useCallback((results) => {
-            console.log("KANE inside the gesture detect top level")
-            let gesture = results.gestures?.[0]?.[0];
+            //validate hand
+            const handGesture = results.handResults.gestures?.[0]?.[0]; // hand gesture
+            const isHandGestureValid = handGesture && handGesture.score > 0.6 && handGesture.categoryName == "Open_Palm"; // hand gesture validation
+            
+            //validate face
+            const faceGestureCategories = results.faceResults.faceBlendshapes?.[0]?.categories; // face gesture
+            const smileLeft = faceGestureCategories?.find(c => c.categoryName == "mouthSmileLeft").score; // left half of smile
+            const smileRight = faceGestureCategories?.find(c => c.categoryName == "mouthSmileRight").score; // right half of smile
+            const isFaceGestureValid = faceGestureCategories && ((smileLeft + smileRight) / 2) > 0.6; // face gesture validation
 
             //make sure the correct & its a high quality
-            if (gesture && gesture.score > 0.6 && gesture.categoryName == "Open_Palm") {
-                console.log("KANE...", gesture.categoryName);
+            if (isHandGestureValid && isFaceGestureValid) {
+
+                //log out result for success case
+                console.log("KANE... hand", isHandGestureValid);
+                console.log("KANE... face", isFaceGestureValid);
 
                 if (!holdStartRef.current) {
                     holdStartRef.current = Date.now();
@@ -91,9 +101,19 @@ export default function GestureDetector({ valDetect }) {
                         return;
                     }
 
+                    if (!gestureRecognizer || !faceLandmarker) {
+                        requestAnimationFrame(performDetectionLoop);
+                        return;
+                    }
+
                     const nowInMs = Date.now();
-                    const handResults = gestureRecognizer.recognizeForVideo(video, nowInMs);
-                    onResults(handResults);
+                    const handResults = gestureRecognizer.recognizeForVideo(video, nowInMs); // get hand gestures
+                    const faceResults = faceLandmarker.detectForVideo(video, nowInMs); // get face gestures
+                    const videoResults = {
+                        "handResults": handResults,
+                        "faceResults": faceResults
+                    }
+                    onResults(videoResults); // send the combines results to the onResult fcn
 
                     requestAnimationFrame(performDetectionLoop);
                 };
